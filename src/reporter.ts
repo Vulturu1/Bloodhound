e74406b553c3044d338c2df5d85882114782a1d1
Vulturu1 <realgmocanu@gmail.com>
2026-04-26 15:32:34 -0400

    Added formatByType function.

--- src/reporter.ts
+++ src/reporter.ts
@@ -1,6 +1,33 @@
 import * as fs from "fs";
 import { Annotation, AnnotationType } from "./types";
 
-export function formatByType(annotations: Annotation[]) {
-    
+interface AnnotationGroup {
+    annotationType: AnnotationType;
+    groupedAnnotations: Annotation[];
 }
+
+export function formatByType(annotations: Annotation[]): AnnotationGroup[] {
+    const grouped: AnnotationGroup[] = []
+    for (const annoType of Object.values(AnnotationType)) {
+        const outputAnnos = annotations.filter(annotation => annotation.annotationType === annoType);
+        if (!outputAnnos) { continue; }
+        const annoGroup: AnnotationGroup = {
+            annotationType: annoType,
+            groupedAnnotations: outputAnnos
+        }
+        grouped.push(annoGroup);
+    }
+    return grouped;
+}
+
+export function formatByFile(annotations: Annotation[]) {
+    const uniquePaths = [...new Set(annotations.map(a => a.filePath))];
+}
+
+//console.log(`=== ${outputAnnos.length} ${annoType}'s Found ================`)
+//        for (const annotation of outputAnnos) {
+//            console.log("\n");
+//            console.log(`Annotation Type | ${annotation.annotationType} on line ${annotation.annotationLine}`);
+//            console.log(`Comment         | ${annotation.comment}`);
+//            console.log(`Path to file    | ${annotation.filePath}`);
+//        }
